cfe7e68db977010a091ddb50357b43018c650159
Hov26 <hov.ghevondyan1994@gmail.com>
2021-04-20 14:29:14 +0400

    Fix: js, add slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,44 +1,75 @@
-// Show 3 news card onCLick
-document.getElementById('load').onclick = () => {
-    const items = document.getElementsByClassName('hide');
-
-    for (i = 0; i < 6; i++) {
-        if (items[i]) {
-            items[i].classList.remove('hide');
-        }
-    }
-};
+function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+}
 
 $(document).ready(function() {
 
+    // copy hero button text
+    $('.hero-btn').click(function() {
+        myFunction()
+    })
+
+    // Hide ecosystem section cards on mobile
+    var windowWidth = $(window).width();
+    if (windowWidth < 1024) {
+        $('.ecosystem .card:nth-child(n+5)').addClass('hide');
+        $('#loadEco').click(function() {
+            var ecoCards = $('.card.hide')
+            for (i = 0; i < ecoCards.length; i++) {
+                if (ecoCards[i]) {
+                    ecoCards[i].classList.remove('hide');
+                }
+            }
+            if (!$('.card').hasClass('hide')) {
+                $(this).hide()
+            }
+        })
+    }
+
+    // Scroll page onClicking hero arrow
     $('.hero-scroll-btn').click(function() {
         $("html, body").animate({ scrollTop: $('.why-kornia').offset().top }, 1000);
     })
 
-
+    // open menu
     $('.hamburger').click(function() {
         $('.nav').addClass('open')
         $('.owl-carousel').addClass('off')
     })
+
+    // close menu
     $('.close-btn').click(function() {
         $('.nav').removeClass('open')
     })
 
-    // var newsSlider = $('.owl-carousel');
 
-    // newsSlider.owlCarousel({
-    //     nav: false,
-    //     items: 2,
-    //     loop: true,
-    //     autoplay: true,
-    //     autoplayTimeout: 5000,
-    //     smartSpeed: 800,
-    //     autoplayHoverPause: true,
-    //     responsive: {
-    //         // breakpoint from 768 up
-    //         200: {
-    //             items: 2,
-    //         }
-    //     }
-    // });
+    // Show 3 news card onCLick
+    $('#load').click(function() {
+        // load news card onCLick "load"
+
+        var newsCards = $('.news-card.hide')
+        for (i = 0; i < 6; i++) {
+            if (newsCards[i]) {
+                newsCards[i].classList.remove('hide');
+            }
+        }
+        if (!$('.news-card').hasClass('hide')) {
+            $(this).hide()
+        }
+    })
+
+    // init slider
+    var slider = $('.owl-carousel');
+    slider.owlCarousel({
+        nav: false,
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 800,
+        autoplayHoverPause: true
+    });
 })
